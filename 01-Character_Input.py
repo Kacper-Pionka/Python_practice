0262237929c27e81dfc1276421d9e80953504529
Kacper-Pionka <78038991+Kacper-Pionka@users.noreply.github.com>
2021-01-26 22:26:36 +0100

    Update 01-Character_Input.py

--- 01-Character_Input.py
+++ 01-Character_Input.py
@@ -7,5 +7,3 @@ text2 = 'And what is your age?\n'
 age = input(text2)
 
 print('Well it seems that in ', 100 - int(age), 'years youll be 100 years old!')
-multip = int(input('how many times '))
-print('gugu gaga\n' * multip)
